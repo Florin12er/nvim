a513d11c3a7addcee9b279fa7f1f0239acaa296b
Florin12er <142166195+Florin12er@users.noreply.github.com>
2024-01-16 20:38:25 +0100

    add a comment plugin

--- lua/FlorinVim/packer.lua
+++ lua/FlorinVim/packer.lua
@@ -1,93 +1,99 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    {
-        "stevearc/conform.nvim",
-        event = { "BufReadPre", "BufNewFile" },
-    },
+	{
+		"stevearc/conform.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+	},
 
-    "wbthomason/packer.nvim",
-    {
-        "folke/tokyonight.nvim",
-        lazy = false,
-        priority = 1000,
-        opts = {},
-    },
-    "maxmellon/vim-jsx-pretty",
-    "onsails/lspkind.nvim",
-    {
-        'barrett-ruth/live-server.nvim',
-        build = 'npm global add live-server',
-        config = true
-    },
-    { "rafamadriz/friendly-snippets" },
-    {
-        "nvim-telescope/telescope.nvim",
-        tag = "0.1.5",
-        dependencies = { "nvim-lua/plenary.nvim" },
-    },
-    {
-        "nvim-lualine/lualine.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-    },
-    {
-        "jsongerber/nvim-px-to-rem",
-        config = true,
-    },
-    { "kevinhwang91/nvim-ufo",       dependencies = "kevinhwang91/promise-async" },
-    {
-        "Exafunction/codeium.vim",
-        config = function()
-            vim.keymap.set("i", "<C-g>", function()
-                return vim.fn["codeium#Accept"]()
-            end, { expr = true, silent = true })
-            vim.keymap.set("i", "<c-;>", function()
-                return vim.fn["codeium#CycleCompletions"](1)
-            end, { expr = true, silent = true })
-            vim.keymap.set("i", "<c-,>", function()
-                return vim.fn["codeium#CycleCompletions"](-1)
-            end, { expr = true, silent = true })
-            vim.keymap.set("i", "<c-x>", function()
-                return vim.fn["codeium#Clear"]()
-            end, { expr = true, silent = true })
-        end,
-    },
-    "ThePrimeagen/vim-be-good",
-    "nvim-lua/plenary.nvim",
-    "brenoprata10/nvim-highlight-colors",
-    "nvim-ts-autotag",
-    "nvim-treesitter/nvim-treesitter",
-    "windwp/nvim-ts-autotag",
-    "ThePrimeagen/harpoon",
-    "mbbill/undotree",
-    "tpope/vim-fugitive",
-    "norcalli/nvim-colorizer.lua",
-    {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "neovim/nvim-lspconfig",
-    },
-    { "hrsh7th/nvim-cmp", "hrsh7th/cmp-nvim-lsp" },
-    "nvim-tree/nvim-tree.lua",
-    "nvim-tree/nvim-web-devicons",
-    "dcampos/nvim-snippy",
-    "dcampos/cmp-snippy",
-    {
-        "rebelot/terminal.nvim",
-        config = function()
-            require("terminal").setup()
-        end,
-    },
+	"wbthomason/packer.nvim",
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	},
+	"maxmellon/vim-jsx-pretty",
+	"onsails/lspkind.nvim",
+	{
+		"barrett-ruth/live-server.nvim",
+		build = "npm global add live-server",
+		config = true,
+	},
+	{ "rafamadriz/friendly-snippets" },
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
+	{
+		"jsongerber/nvim-px-to-rem",
+		config = true,
+	},
+	{ "kevinhwang91/nvim-ufo", dependencies = "kevinhwang91/promise-async" },
+	{
+		"Exafunction/codeium.vim",
+		config = function()
+			vim.keymap.set("i", "<C-g>", function()
+				return vim.fn["codeium#Accept"]()
+			end, { expr = true, silent = true })
+			vim.keymap.set("i", "<c-;>", function()
+				return vim.fn["codeium#CycleCompletions"](1)
+			end, { expr = true, silent = true })
+			vim.keymap.set("i", "<c-,>", function()
+				return vim.fn["codeium#CycleCompletions"](-1)
+			end, { expr = true, silent = true })
+			vim.keymap.set("i", "<c-x>", function()
+				return vim.fn["codeium#Clear"]()
+			end, { expr = true, silent = true })
+		end,
+	},
+	"ThePrimeagen/vim-be-good",
+	"nvim-lua/plenary.nvim",
+	"brenoprata10/nvim-highlight-colors",
+	"nvim-ts-autotag",
+	"nvim-treesitter/nvim-treesitter",
+	"windwp/nvim-ts-autotag",
+	"ThePrimeagen/harpoon",
+	"mbbill/undotree",
+	"tpope/vim-fugitive",
+	"norcalli/nvim-colorizer.lua",
+	{
+		"numToStr/Comment.nvim",
+		opts = {
+			-- add any options here
+		},
+		lazy = false,
+	},
+	{
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
+	},
+	{ "hrsh7th/nvim-cmp", "hrsh7th/cmp-nvim-lsp" },
+	"nvim-tree/nvim-tree.lua",
+	"nvim-tree/nvim-web-devicons",
+	"dcampos/nvim-snippy",
+	"dcampos/cmp-snippy",
+	{
+		"rebelot/terminal.nvim",
+		config = function()
+			require("terminal").setup()
+		end,
+	},
 })
-
--- lua/FlorinVim/remap.lua
+++ lua/FlorinVim/remap.lua
@@ -32,11 +32,6 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_dd]])
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
-vim.keymap.set("n", "<leader><leader>", function()
-    vim.cmd("so")
+vim.keymap.set("n", "<leader>so", function()
+	vim.cmd("so")
 end)
-
